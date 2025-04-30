ceebcba63f65021abf70db7c19ee918e52a7fdf0
Huertz <104065687+Huertz@users.noreply.github.com>
2025-04-30 02:19:51 -0500

    removed props

--- src/App.js
+++ src/App.js
@@ -64,11 +64,8 @@ function App() {
         </button>
 
         <Header />
-        <Main
-          posts={searchedPosts}
-          onAddPost={handleAddPost}
-        />
-        <Archive onAddPost={handleAddPost} />
+        <Main />
+        <Archive />
         <Footer />
       </section>
     </PostContext.Provider>
@@ -108,24 +105,25 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main({ posts, onAddPost }) {
+function Main() {
   return (
     <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
+      <FormAddPost />
+      <Posts />
     </main>
   );
 }
 
-function Posts({ posts }) {
+function Posts() {
   return (
     <section>
-      <List posts={posts} />
+      <List />
     </section>
   );
 }
 
-function FormAddPost({ onAddPost }) {
+function FormAddPost() {
+  const { onAddPost } = useContext(PostContext);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
 
@@ -154,7 +152,8 @@ function FormAddPost({ onAddPost }) {
   );
 }
 
-function List({ posts }) {
+function List() {
+  const { posts } = useContext(PostContext);
   return (
     <ul>
       {posts.map((post, i) => (
@@ -167,7 +166,8 @@ function List({ posts }) {
   );
 }
 
-function Archive({ onAddPost }) {
+function Archive() {
+  const { onAddPost } = useContext(PostContext);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
